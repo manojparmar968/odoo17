84e03916ec8115a005bcc9ca4a34feb464ae09d3
Manoj Parmar <manojparmar968@gmail.com>
2024-01-21 13:23:59 +0530

    owl static todo app

---
+++ owl1/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    "name": "Owl Tutorial v17",
+    "version": '1.0',
+    "category": "Interview/others",
+    "license": "AGPL-3",
+    "summary": "Owl Tutorial ",
+    'description':"""
+    owl
+    """,
+    "author": "Manoj Parmar",
+    "maintainers": ["Manoj Parmar"],
+    "website": "www.xyz.com",
+    "depends": ['base_setup','base', 'web', 'sale', 'board'],
+    "data": [
+        # "security/ir.model.access.csv",
+        # "views/.xml",
+        # "report/.xml",
+        # "data/.xml",
+    ],
+    "installable": True,
+    'auto_install': False,
+    "application": True,
+    'assets': {
+        'web.assets_backend': [
+        ],
+        # 'web.assets_web_dark': [],
+        # 'web.assets_frontend': [],
+    },
+}
