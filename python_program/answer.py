9d59670ec58b97b55e26fc94cecea31e62319060
Manoj Parmar <manojparmar968@gmail.com>
2024-01-29 00:39:07 +0530

    search_read()

---
+++ python_program/answer.py
@@ -0,0 +1,26 @@
+"""
+1
+2 9
+3 8 10
+4 7 11 14
+5 6 12 13 15
+"""
+n = 5
+# for i in range(n):
+#     for j in range(i + 1):
+#         x = 0
+#         for k in range(j):
+#             x = x + n - k
+#             if j % 2 == 0:
+#                 print(x + i - j + 1,end=" ")
+#             else:
+#                 print(x + n - i,end=" ")
+#         print()
+# print("\n")
+
+for i in range(n):
+    for j in range(i + 1):
+        # if j % 2 == 0:
+            print(i - j + 1,end=" ")
+        
+    print()
