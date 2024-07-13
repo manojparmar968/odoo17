8f0a6b08e37927af34a4ae4fb2a423b3e6553fb2
Manoj Parmar <manojparmar968@gmail.com>
2024-07-13 18:11:48 +0530

    basic

---
+++ python_program/python_inbuilt_fun.py
@@ -0,0 +1,13 @@
+# The floor() function:
+# floor() method in Python returns the floor of x i.e., the largest integer not greater than x.
+import math
+print("math.floor(-23.11) : ", math.floor(-23.11))
+print("math.floor(300.16) : ", math.floor(300.16))
+print("math.floor(300.72) : ", math.floor(300.72))
+
+# The ceil() Function:
+# The method ceil(x) in Python returns a ceiling value of x i.e., the smallest integer greater than or equal to x.
+import math
+print("math.ceil(-23.11) : ", math.ceil(-23.11))
+print("math.ceil(300.16) : ", math.ceil(300.16))
+print("math.ceil(300.72) : ", math.ceil(300.72))
